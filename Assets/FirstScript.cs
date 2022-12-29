1664595fb2419753ef32e0f0eac05fa0898d176d
DESKTOP-QCUULMR\Admin <locduoi1998>
2022-12-29 13:56:22 +0700

    First commit,

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class FirstScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("sldhjflksdj");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Time.deltaTime);
+        Debug.Log("Update frame when running: ");
+    }
+}
